74c383de1fb9bbd174c57e2dd4be70691d4e969b
DirtyCode47 <alexprog47@mail.ru>
2024-04-16 07:24:12 +1000

    Добавил класс игрока

---
+++ Blackjack_console/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_console
+{
+    internal class Player
+    {
+        public CardStack hand;
+        public int currentPoints;
+
+        public Player()
+        {
+            hand = new CardStack();
+            currentPoints = 5000;
+        }
+    }
+}
